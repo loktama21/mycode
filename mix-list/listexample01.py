535773afdf518c8a3bfda798c19690520a9812fc
loktama21 <lb.tamang77@gmail.com>
2023-01-10 18:43:07 +0000

    manipulating lists

---
+++ mix-list/listexample01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+wordbank= ["indentation", "spaces"] 
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+wordbank.append(4)
+num= int(input("Pick a student number!"))
+#choice= int(input("Pick a student number!"))
+student_name= tlgstudents[num]
+print(string(student_name) + " always uses " + string(wordbank[2] + " " + wordbank[1] + " to indent")
