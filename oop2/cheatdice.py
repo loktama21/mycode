60aea79c75a2ec1de92b415d714e226fa15b1c2d
loktama21 <lb.tamang77@gmail.com>
2023-01-13 20:17:00 +0000

    utilizing classes

--- oop2/cheatdice.py
+++ oop2/cheatdice.py
@@ -32,4 +32,11 @@ class Cheat_Loaded_Dice(Player): # inheritance of Player
             if self.dice[i] < 6:
                 self.dice[i] += 1
             i += 1
+# if total dice is under 9, get a re-roll
+class Cheat_Mulligan(Player):
+    def cheat(self):
+       if sum(self.dice) <= 9:
+           self.dice = []
+           for i in range(3):
+              self.dice.append(random.randint(1,6))
 
